16fba2b6957ca5e0e426f3d2c48b0c0dd3ef43c8
Sofiane <sofiane.benembareck@hotmail.com>
2021-05-28 18:06:11 +0200

    [WIP] Define StatModifier class
    Define method we will use in stat class

--- Knight-VS-Orc/StatModifier.cpp
+++ Knight-VS-Orc/StatModifier.cpp
@@ -1,1 +1,24 @@
 ﻿#include "StatModifier.h"
+#include "iostream"
+
+float StatModifier::GetValue() const
+{
+	switch (this->type)
+	{
+	case Multiplier:
+		return this->value;
+		break;
+	case PercentMultiplier:
+		return this->value * 0.01f;
+		break;
+	}
+
+	return this->value;
+}
+
+void StatModifier::swap(StatModifier& sm)
+{
+	std::swap(this->value, sm.value);
+	std::swap(this->type, sm.type);
+	std::swap(this->executionOrder, sm.executionOrder);
+}
--- Knight-VS-Orc/StatModifier.h
+++ Knight-VS-Orc/StatModifier.h
@@ -1,26 +1,44 @@
 ﻿#pragma once
 
-
 enum StatModifierType
 {
-	Multiplier, // Flat value which multiply the stat value
-	PercentMultiplier // Value in percent, which modify the stat by it's value
+	Multiplier = 0, // Flat value which multiply the stat value
+	PercentMultiplier = 10 // Value in percent, between 0 and float max value, which modify the stat by it's value
 };
 
 class StatModifier
 {
 public:
-	StatModifier() : type(StatModifierType::Multiplier)
+#pragma region Constructor
+	StatModifier() : type(StatModifierType::Multiplier), value(0.0f), executionOrder(static_cast<int>(StatModifierType::Multiplier)){}
+	StatModifier(StatModifierType _type, float _value) : type(_type), value(_value), executionOrder(static_cast<int>(_type)) {}
+	StatModifier(StatModifierType _type, float _value, int _executionOrder) : type(_type), value(_value), executionOrder(_executionOrder){}
+	StatModifier(const StatModifier& sm) : type(sm.type), value(sm.value), executionOrder(sm.executionOrder){}
+
+	StatModifier& operator=(StatModifier sm)
 	{
-		
+		this->swap(sm);
+		return *this;
 	}
+#pragma endregion 
+
+	~StatModifier() = default;
 	
 	StatModifierType GetModifierType() const
 	{
 		return this->type;
 	}
+	float GetValue() const;
+	
+	int GetExecutionOrder() const
+	{
+		return this->executionOrder;
+	}
 	
 private:
 	StatModifierType type;
+	float value;
+	int executionOrder;
 	
+	void swap(StatModifier& sm);
 };
