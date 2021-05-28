415e91a4a1ac68893e301f8c7fbb63db0f25e0c7
Sofiane <sofiane.benembareck@hotmail.com>
2021-05-28 17:47:41 +0200

    [WIP] Define base Stat class
    Add base methods to the stat class
    Create enum StatModifierType to handle different type of modifier

--- Knight-VS-Orc/Stat.cpp
+++ Knight-VS-Orc/Stat.cpp
@@ -1,1 +1,51 @@
 ﻿#include "Stat.h"
+
+#include "StatModifier.h"
+
+
+void Stat::swap(Stat& s)
+{
+	std::swap(this->baseValue, s.baseValue);
+	std::swap(this->modifiers, s.modifiers);
+}
+
+void Stat::AddModifier(StatModifier _modifier)
+{
+	
+}
+
+void Stat::AddMultipleModifier(std::vector<StatModifier> _modifiers)
+{
+	
+}
+
+float Stat::GetValue()
+{
+	return CalculateRealValue();
+}
+
+float Stat::CalculateRealValue() const
+{
+	// Initialize a value to the base value
+	float realValue = this->baseValue;
+
+	// Process all modifiers
+	for(auto& mod : this->modifiers)
+	{
+		switch (mod.GetModifierType())
+		{
+		case StatModifierType::Multiplier:
+			break;
+			
+		case StatModifierType::PercentMultiplier:
+			break;
+		} 
+	}
+	
+	return realValue;
+}
+
+void Stat::UpdateModifiers()
+{
+	
+}
--- Knight-VS-Orc/StatModifier.h
+++ Knight-VS-Orc/StatModifier.h
@@ -1,7 +1,26 @@
 ﻿#pragma once
 
+
+enum StatModifierType
+{
+	Multiplier, // Flat value which multiply the stat value
+	PercentMultiplier // Value in percent, which modify the stat by it's value
+};
+
 class StatModifier
 {
 public:
+	StatModifier() : type(StatModifierType::Multiplier)
+	{
+		
+	}
+	
+	StatModifierType GetModifierType() const
+	{
+		return this->type;
+	}
+	
+private:
+	StatModifierType type;
 	
 };
