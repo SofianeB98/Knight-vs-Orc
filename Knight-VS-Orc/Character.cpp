44d48cb32fdf0189da35cec41824e27fc4a2c6bc
Sofiane <sofiane.benembareck@hotmail.com>
2021-05-29 09:06:19 +0200

    [WIP] Define base Character class & methods + comment class
    Base methods are defined with the actual project resources. Missing methods will be defined in the future

--- Knight-VS-Orc/Character.cpp
+++ Knight-VS-Orc/Character.cpp
@@ -1,1 +1,128 @@
 ﻿#include "Character.h"
+
+#include "Weapon.h"
+#include "Ability.h"
+#include "Status.h"
+
+Character::Character() : life(10), damageMultiplier(1.0f)
+{
+	this->status.reserve(5);
+	this->weapon = nullptr;
+	this->ability = nullptr;
+}
+
+Character::Character(const Character& c) : life(c.life), damageMultiplier(c.damageMultiplier), weapon(c.weapon), ability(c.ability), status(c.status)
+{
+}
+
+Character& Character::operator=(Character c)
+{
+	swap(c);
+	return *this;
+}
+
+
+Character::~Character()
+{
+	if(this->weapon != nullptr)
+	{
+		delete this->weapon;
+		this->weapon = nullptr;
+	}
+
+	if (this->ability != nullptr)
+	{
+		delete this->ability;
+		this->ability = nullptr;
+	}
+
+	ClearStatus();
+}
+
+
+void Character::UpdateCharacterFields()
+{
+	// Update all stat modifiers
+	this->life.UpdateModifiers();
+	this->damageMultiplier.UpdateModifiers();
+
+	// Update Ability cooldown
+
+
+	// Update Status cooldown
+	UpdateStatus();
+}
+
+void Character::UpdateStatus()
+{
+	for (auto& st : status)
+	{
+
+	}
+
+	// Remove all modifier with a 0 durability
+	const auto removed = std::remove_if(this->status.begin(), this->status.end(), [](Status* sm)
+		{
+			if (sm != nullptr)
+			{
+				if (0)
+				{
+					delete sm;
+					sm = nullptr;
+					return true;
+				}
+
+				return false;
+			}
+
+			return true;
+		});
+
+	// "Resize" the vector to remove useless removed value
+	this->status.resize(std::distance(this->status.begin(), removed));
+}
+
+void Character::ClearStatus()
+{
+	for (auto& st : this->status)
+	{
+		if (st != nullptr)
+		{
+			delete st;
+			st = nullptr;
+		}
+	}
+
+	this->status.clear();
+}
+
+
+void Character::UseAbility(Character& target)
+{
+	
+}
+
+void Character::UseWeapon(Character& target)
+{
+	
+}
+
+void Character::ApplyStatus(Status* s)
+{
+	this->status.emplace_back(s);
+}
+
+void Character::TakeDamage(float _damage)
+{
+	
+}
+
+
+void Character::swap(Character& c)
+{
+	std::swap(this->life, c.life);
+	std::swap(this->damageMultiplier, c.damageMultiplier);
+	std::swap(this->ability, c.ability);
+	std::swap(this->weapon, c.weapon);
+	std::swap(this->status, c.status);
+}
--- Knight-VS-Orc/Character.h
+++ Knight-VS-Orc/Character.h
@@ -1,7 +1,102 @@
 ﻿#pragma once
 
+#include "Stat.h"
+
+// Forward declaration
+class Status;
+class Weapon;
+class Ability;
+
+/// <summary>
+/// Character is the base class for every character like Knight, Orc, Healer, ...
+/// </summary>
 class Character
 {
 public:
+#pragma region Constructor
+	/// <summary>
+	/// Default constructor of Character class
+	/// </summary>
+	Character();
+
+	/// <summary>
+	/// Copy constructor of Character Class
+	/// </summary>
+	/// <param name="c"></param>
+	Character(const Character& c);
+
+	/// <summary>
+	/// = operator of character class
+	/// </summary>
+	/// <param name="c"></param>
+	/// <returns></returns>
+	Character& operator=(Character c);
+#pragma endregion 
+
+	~Character();
+
+#pragma region Character Method
+	/// <summary>
+	/// Call this method to try to use character ability on the specific target
+	/// </summary>
+	/// <param name="target"></param>
+	virtual void UseAbility(Character& target);
+
+	/// <summary>
+	/// Call this method to use character weapon on the specific target
+	/// </summary>
+	/// <param name="target"></param>
+	virtual void UseWeapon(Character& target);
+
+	// Call by Use Ability, but make this method public because
+	// we can imagine in a futur update, other object can apply a status
+	/// <summary>
+	/// This method apply a specific Status on this character
+	/// </summary>
+	/// <param name="target"></param>
+	virtual void ApplyStatus(Status* s);
+#pragma endregion 
 	
+	/// <summary>
+	/// Call when a turn finish
+	/// Update all modifiers, cooldown and durability of every fields
+	/// </summary>
+	virtual void UpdateCharacterFields();
+	
+protected:
+	// Base Stats of every Character
+	Stat life;
+	Stat damageMultiplier;
+	
+	// Item & Ability of a character
+	Weapon* weapon;
+	Ability* ability;
+
+	// Character status
+	std::vector<Status*> status;
+
+	/// <summary>
+	/// Update all status durability
+	/// And remove all status with 0 durability
+	/// </summary>
+	virtual void UpdateStatus();
+
+	/// <summary>
+	/// Apply damage to this character
+	/// </summary>
+	/// <param name="_damage"></param>
+	virtual void TakeDamage(float _damage);
+
+	// Copy & Swap, this method is used by "= operator"
+	/// <summary>
+	/// Swap this class field with parameter value
+	/// </summary>
+	/// <param name="c"></param>
+	virtual void swap(Character& c);
+private:
+
+	/// <summary>
+	/// Remove and destroy all status
+	/// </summary>
+	void ClearStatus();
 };
