6fba47ecfda5025eb9fde561babae1999e61b487
Sofiane <sofiane.benembareck@hotmail.com>
2021-05-29 15:12:09 +0200

    Create default weapon class + add construtor into Character class

--- Knight-VS-Orc/Weapon.cpp
+++ Knight-VS-Orc/Weapon.cpp
@@ -1,1 +1,31 @@
 ﻿#include "Weapon.h"
+
+Weapon::Weapon() : damage(1)
+{
+	
+}
+
+Weapon::Weapon(float damage) : damage(damage)
+{
+}
+
+Weapon::Weapon(const Weapon& w) : damage(w.damage)
+{
+}
+
+Weapon& Weapon::operator=(Weapon w)
+{
+	swap(w);
+	return *this;
+}
+
+void Weapon::swap(Weapon& w)
+{
+	std::swap(this->damage, w.damage);
+}
+
+
+float Weapon::GetDamage() const
+{
+	return this->damage.GetValue();
+}
--- Knight-VS-Orc/Weapon.h
+++ Knight-VS-Orc/Weapon.h
@@ -1,7 +1,53 @@
 ﻿#pragma once
+#include "Stat.h"
 
 class Weapon
 {
 public:
+#pragma region Constructor
+	/// <summary>
+	/// Default construtor of Weapon, create a weapon with damage as 1
+	/// </summary>
+	Weapon();
+
+	/// <summary>
+	/// Create a weapon with the specific damage value
+	/// </summary>
+	/// <param name="damage"></param>
+	Weapon(float damage);
+
+	/// <summary>
+	/// Copy constructor of Weapon Class
+	/// </summary>
+	/// <param name="w"></param>
+	Weapon(const Weapon& w);
+
+	/// <summary>
+	/// = operator of wepaon class
+	/// </summary>
+	/// <param name="w"></param>
+	/// <returns></returns>
+	Weapon& operator=(Weapon w);
+#pragma endregion
+
+	~Weapon() = default;
+
+	/// <summary>
+	/// Return the weapon damage with applied modifier
+	/// </summary>
+	/// <returns></returns>
+	virtual float GetDamage() const;
+	
+protected:
+	// Store the weapon damage
+	Stat damage;
+
+	// Copy & Swap, this method is used by "= operator"
+	/// <summary>
+	/// Swap this class field with parameter value
+	/// </summary>
+	/// <param name="w"></param>
+	virtual void swap(Weapon& w);
+private:
 	
 };
