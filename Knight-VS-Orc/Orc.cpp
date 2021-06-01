c23244e77bafbdc43c45ebbd8fbda87de07141a4
Sofiane <sofiane.benembareck@hotmail.com>
2021-06-01 08:11:08 +0200

    Add base method & constructor for Orc Character Class

--- Knight-VS-Orc/Orc.cpp
+++ Knight-VS-Orc/Orc.cpp
@@ -1,1 +1,20 @@
 ﻿#include "Orc.h"
+
+Orc::Orc() : Character()
+{
+}
+
+Orc::Orc(Weapon& _w, Ability& _a) : Character(_w, _a)
+{
+}
+
+Orc::Orc(const Orc& _c) : Character(_c)
+{
+	
+}
+
+Orc& Orc::operator=(Orc _c)
+{
+	Character::swap(_c);
+	return *this;
+}
--- Knight-VS-Orc/Orc.h
+++ Knight-VS-Orc/Orc.h
@@ -4,5 +4,34 @@
 class Orc : public Character
 {
 public:
+#pragma region Constructor
+	/// <summary>
+	/// Default Orc constructor
+	/// Life = 60
+	/// </summary>
+	Orc();
+
+	/// <summary>
+	/// Create a Orc like it's default constructor with
+	/// specific ability and weapon
+	/// </summary>
+	/// <param name="_w"></param>
+	/// <param name="_a"></param>
+	Orc(Weapon& _w, Ability& _a);
+
+	/// <summary>
+	/// Copy constructor
+	/// </summary>
+	/// <param name="_c"></param>
+	Orc(const Orc& _c);
+
+	/// <summary>
+	/// = operator of this class
+	/// </summary>
+	/// <param name="_c"></param>
+	/// <returns></returns>
+	Orc& operator=(Orc _c);
+#pragma endregion
+
 	
 };
