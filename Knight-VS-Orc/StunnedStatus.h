c7eee2ddbc193ba26c8a4cf41d3565e4a550b1c7
Sofiane <sofiane.benembareck@hotmail.com>
2021-06-01 18:54:07 +0200

    Update Stunned Status class name + Define Process method

--- Knight-VS-Orc/StunnedStatus.h
+++ Knight-VS-Orc/StunnedStatus.h
@@ -1,41 +1,41 @@
 ﻿#pragma once
 #include "Status.h"
 
-class StunedStatus : public Status
+class StunnedStatus : public Status
 {
 public:
 #pragma region Constructor
 	/// <summary>
 	/// Create default stuned status with durability = 1 and type = battle turn
 	/// </summary>
-	StunedStatus();
+	StunnedStatus();
 
 	/// <summary>
 	/// Create stuned status with specific durabilty
 	/// type = begin turn
 	/// </summary>
 	/// <param name="_durability"></param>
-	StunedStatus(int _durability);
+	StunnedStatus(int _durability);
 
 	/// <summary>
 	/// Create stuned status with specific durability and type
 	/// </summary>
 	/// <param name="_durability"></param>
 	/// <param name="_statusType"></param>
-	StunedStatus(int _durability, StatusType _statusType);
+	StunnedStatus(int _durability, StatusType _statusType);
 
 	/// <summary>
 	/// Copy the parameter
 	/// </summary>
 	/// <param name="_s"></param>
-	StunedStatus(const StunedStatus& _s);
+	StunnedStatus(const StunnedStatus& _s);
 
 	/// <summary>
 	/// = Operator
 	/// </summary>
 	/// <param name="_s"></param>
 	/// <returns></returns>
-	StunedStatus& operator=(StunedStatus _s);
+	StunnedStatus& operator=(StunnedStatus _s);
 #pragma endregion
 	
 	/// <summary>
