68570d4811b09572212ca55d3e47457e01c07864
Sofiane <sofiane.benembareck@hotmail.com>
2021-06-02 10:32:56 +0200

    [WIP] Define Start & Update method of GameObject class
    these method call start & update method of all components attached to this gameobject

--- Knight-VS-Orc/GameObject.cpp
+++ Knight-VS-Orc/GameObject.cpp
@@ -17,15 +17,15 @@ void GameObject::Start()
 {
 	for (auto& component : this->components)
 	{
-
+		component->Start(*this);
 	}
 }
 
-void GameObject::Update(double dt)
+void GameObject::Update(double _dt)
 {
 	for(auto& component : this->components)
 	{
-		
+		component->Update(*this, _dt);
 	}
 }
 
@@ -50,6 +50,8 @@ void GameObject::Destroy()
 template <typename T>
 T& GameObject::AddComponent()
 {
+	static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
+	
 	// We search if the component exist into this components list
 	// if true, return it
 	if (T* objectComponent = GetComponent<T>())
@@ -65,6 +67,8 @@ T& GameObject::AddComponent()
 template <typename T>
 T* GameObject::GetComponent() const
 {
+	static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
+	
 	// Search the first component T
 	auto found = std::find_if(this->components.begin(), this->components.end(), [](Component* component) {
 		return dynamic_cast<T*>(component);
