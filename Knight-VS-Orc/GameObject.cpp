82cec2ac8a7ba6bb031da67580f42090309b7f8e
Sofiane <sofiane.benembareck@hotmail.com>
2021-06-02 09:08:26 +0200

    [WIP] Create methods & constructor for GameObject class

--- Knight-VS-Orc/GameObject.cpp
+++ Knight-VS-Orc/GameObject.cpp
@@ -1,1 +1,75 @@
 ﻿#include "GameObject.h"
+#include "Component.h"
+
+GameObject::GameObject()
+{
+	this->components.reserve(5);
+}
+
+GameObject::~GameObject()
+{
+	Destroy();
+}
+
+
+
+void GameObject::Start()
+{
+	for (auto& component : this->components)
+	{
+
+	}
+}
+
+void GameObject::Update(double dt)
+{
+	for(auto& component : this->components)
+	{
+		
+	}
+}
+
+
+
+void GameObject::Destroy()
+{
+	for (auto& component : this->components)
+	{
+		if (component == nullptr)
+			continue;
+
+		delete component;
+		component = nullptr;
+	}
+
+	this->components.clear();
+}
+
+
+
+template <typename T>
+T& GameObject::AddComponent()
+{
+	// We search if the component exist into this components list
+	// if true, return it
+	if (T* objectComponent = GetComponent<T>())
+		return *objectComponent;
+
+	// else, create a new component and return its reference to modify it
+	T* component = new T;
+	this->components.emplace_back(component);
+
+	return *component;
+}
+
+template <typename T>
+T* GameObject::GetComponent() const
+{
+	// Search the first component T
+	auto found = std::find_if(this->components.begin(), this->components.end(), [](Component* component) {
+		return dynamic_cast<T*>(component);
+		});
+
+	// If we not found a component, return null else return it
+	return found == this->components.end() ? nullptr : static_cast<T*>(*found);
+}
--- Knight-VS-Orc/GameObject.h
+++ Knight-VS-Orc/GameObject.h
@@ -1,7 +1,58 @@
 ﻿#pragma once
+#include <vector>
 
+class Component;
 class GameObject
 {
 public:
+#pragma region Constructor
+	/// <summary>
+	/// Create a default Game Object and reserve its components
+	/// </summary>
+	GameObject();
+#pragma endregion
+
+	~GameObject();
+
+#pragma region Game Logic Method
+	/// <summary>
+	/// 
+	/// </summary>
+	void Start();
+
+	/// <summary>
+	/// Update methode is called once per frame
+	/// </summary>
+	/// <param name="_dt"></param>
+	void Update(double _dt);
+#pragma endregion 
 	
+#pragma region Component Method
+	/// <summary>
+	/// Add Component method add the typename component to the
+	/// Game object components, if doesn't have it, else return the
+	/// Existing component
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <returns></returns>
+	template <typename T>
+	T& AddComponent();
+
+	/// <summary>
+	/// Return the specified component type if exist
+	/// else return nullptr
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <returns></returns>
+	template <typename T>
+	T* GetComponent() const;
+#pragma endregion 
+	
+private:
+	std::vector<Component*> components;
+
+	/// <summary>
+	/// Destroy all components
+	/// </summary>
+	void Destroy();
 };
