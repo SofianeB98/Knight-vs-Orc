c5bc9d4c0fcd9d36f95c94441d08c421d09bbd1d
Sofiane <sofiane.benembareck@hotmail.com>
2021-06-03 19:32:25 +0200

    [WIP] Create Character component + Battle Manager
    - Create character component base abstract classe + default methods
    - Create default file for BattleManagerComponent

---
+++ Knight-VS-Orc/CharacterComponent.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "Component.h"
+
+enum AbilityChoiceState
+{
+	InProgress = -1,
+	DontUseIt = 0,
+	UseIt = 1
+};
+
+class Character;
+class BattleManagerComponent;
+class CharacterComponent : public Component
+{
+public:
+#pragma region Constructor
+	CharacterComponent(BattleManagerComponent& _battleManager, Character& _character);
+#pragma endregion 
+
+	virtual ~CharacterComponent();
+	
+#pragma region Component Callback
+	virtual void OnEnable(GameObject& _gameObject) override = 0;
+	virtual void Start(GameObject& _gameObject) override = 0;
+	virtual void Update(GameObject& _gameObject, double _dt) override = 0;
+	virtual void OnDisable(GameObject& _gameObject) override = 0;
+#pragma endregion 
+
+#pragma region Character Callback
+	void UseAbility(CharacterComponent& _otherCharacter);
+	void UseWeapon(CharacterComponent& _otherCharacter);
+#pragma endregion 
+	
+#pragma region Getter
+	Character& GetCharacter() const;
+	AbilityChoiceState GetAbilityChoice() const;
+#pragma endregion 
+	
+private:
+	BattleManagerComponent& battleManager;
+	
+	Character* character;
+
+	AbilityChoiceState abilityChoiceState;
+};
